bf660905751a0bf0c57b9008812e04b93e3479ca
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-03-19 03:14:31 +0100

    feat: separate and organize the project with clean arch conceptes layers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './infra/http/app.controller';
+import { AppService } from './caso-de-uso/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'estacionamento',
+      entities: [],
+      synchronize: true
+    }),
+  
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
