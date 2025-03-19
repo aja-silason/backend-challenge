9f2e0bda2a6f2283f7087922762221ec23f1f6d8
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-03-19 09:53:33 +0100

    feat: add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './infra/http/app.controller';
 import { AppService } from './caso-de-uso/app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigureModule } from './infra/configure/configure.module';
+import { DatabaseModule } from './infra/database/database.module';
 
 @Module({
   imports: [
-    ConfigureModule
+    ConfigureModule,
+    DatabaseModule
   ],
   controllers: [AppController],
   providers: [AppService],
