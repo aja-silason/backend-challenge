e8f66700ac499a7ca15afe4c6d7ba3c7e046f53b
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-03-19 09:34:27 +0100

    feat: add class validator and way to documentation with the swagger

---
+++ src/main/docs/swagger-config.ts
@@ -0,0 +1,16 @@
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+export const swaggerConfig = (app: NestExpressApplication): void => {
+
+    const config = new DocumentBuilder()
+        .setTitle("Desafio Estacionamento")
+        .setDescription("Desafio da Stetment MC")
+        .setVersion('0.0.1')
+        .addTag("Estacionamento")
+        .build()
+
+        const document = SwaggerModule.createDocument(app, config);
+        SwaggerModule.setup('api/docs', app, document);
+
+}
