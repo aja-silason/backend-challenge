484aec232e89ba7dc8de285c1dee7945f93e936a
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-03-21 00:38:53 +0100

    feat: create a vehicle entity

---
+++ src/domain/vehicle/vehicle/vehicle.ts
@@ -0,0 +1,71 @@
+import { BadRequestException } from "@nestjs/common";
+import { SpaceModel } from "src/domain/space/model/space.model"
+
+export type VehicleProps = {
+    id?: number,
+    brand: string,
+    model: string,
+    color: string,
+    plate: string,
+    type: string,
+    spaceId: SpaceModel
+}
+
+export class Vehicle {
+
+    constructor(private readonly props: VehicleProps){}
+
+    public static create(props: VehicleProps, id?: string){
+
+        this.validate(props);
+
+        return new Vehicle({
+            ...props
+        }) 
+
+    }
+
+    private static validate(props: VehicleProps){
+
+        const isValidate: Array<keyof VehicleProps> = ["brand", "color", "model", "plate", "type", "spaceId"];
+
+        for(const key of isValidate){
+            const value = props[key];
+            if(value == null  || value ==undefined){
+                throw new BadRequestException(`${key} should not be null`);
+            }
+        }
+
+    }
+
+    public get id(){
+        return this.props.id;
+    }
+
+    public get brand(){
+        return this.props.brand;
+    }
+
+    public get color(){
+        return this.props.color;
+    }
+
+    public get model(){
+        return this.props.model;
+    }
+
+    public get plate(){
+        return this.props.plate;
+    }
+
+    public get spaceId(){
+        return this.props.spaceId;
+    }
+
+    public get type(){
+        return this.props.type;
+    }
+
+
+
+}
