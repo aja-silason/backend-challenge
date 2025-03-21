cdafc419901a64a564bc974f3b6a4ee8fc075a93
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-03-21 05:47:57 +0100

    refactor: create a report entity

---
+++ src/domain/report/model/report.model.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'report'})
+export class ReportModel {
+
+    @PrimaryGeneratedColumn({type: 'int'})
+    id?: string
+
+    @Column()
+    vehicle: string
+    
+    @Column({type: 'timestamp', nullable: true, default: null})
+    in_time: Date
+
+    @Column({type: 'timestamp', nullable: true, default: null})
+    out_time: Date
+    
+    @Column()
+    vehicle_id: number
+
+    @Column({name: 'updated_at', type: 'timestamp', nullable: true, default: null})
+    updatedAt: Date
+
+    @Column({name: 'created_at', type: 'timestamp', nullable: true, default: () => 'CURRENT_TIMESTAMP'})
+    createdAt: Date
+
+}
